0b2f15376da0690febf452e5c88f2b0359048233
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-03-12 22:19:41 -0500

    Add logic to track read status when form submitted.

--- javaScript.js
+++ javaScript.js
@@ -1,15 +1,36 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     
-const myLibrary = [];
 
-function Book() {
-  // the constructor...
-}
 
-function addBookToLibrary() {
-  // do stuff here
-}
+    const myLibrary = [];
 
+    function Book() {
+    // the constructor...
+    }
+
+    function addBookToLibrary() {
+    // do stuff here
+    }
+
+
+    const addBook = document.getElementById('addBook');
+    const title = document.getElementById('title');
+    const author = document.getElementById('author');
+    const pages = document.getElementById('pages');
+
+
+    document.getElementById("myForm").addEventListener("submit", function(event) {
+        event.preventDefault(); // Prevent the default form submission
+        
+        // Get the value of the checked radio button
+        let selectedReadOrNot = document.querySelector('input[name="readOrNot"]:checked');
+      
+        if(selectedReadOrNot) {
+          alert("Read Status: " + selectedReadOrNot.value);
+        } else {
+          alert("Please select if read or unread.");
+        }
+      });
 
 });
